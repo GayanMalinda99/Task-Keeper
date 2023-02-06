ec2a2220b417bc08893340f2407239b2fb22979f
GayanMalinda99 <gayan1999malinda@gmail.com>
2023-02-06 12:41:49 +0530

    Complete HomePage

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -2,23 +2,39 @@ import React, {useState} from "react";
 import ShowTask from "../components/showTask";
 import { Link } from 'react-router-dom';
 import { useDispatch } from "react-redux";
+import { setEdit } from "../actions/taskAction";
 
 const HomePage = () => {
+  const [showMessage, setShowMessage] = useState(false);
+  const dispatch = useDispatch();
+  const handleButtonClick = () => {
+    dispatch(setEdit(null));
+  };
+
   return (
     <div className="row mt-5">
+      {showMessage 
+      ? <div style={{margineLeft:"50%", color:"red"}}>
+        <h5>Task Deleted Successfully</h5>
+      </div> : ""}
       <div style={{ alignItems: "center" }}>
         <div className="d-flex justify-content-center title ">
             <p className="m-3 mt-4 mb-4"><b>TASK KEEPER</b></p>
         </div>
         <div className="my-4" style={{ marginLeft: "46.5%" }}>
           <Link to="/tasks/add">
-            <button className="btn btn-primary btn-sm m-2 btn-radious justify-content-center">
+            <button 
+              className="btn btn-primary btn-sm m-2 btn-radious justify-content-center"
+              onClick={handleButtonClick}
+            >
               Add Task
             </button>
           </Link>    
         </div>
       </div>
-      <ShowTask />
+      <div className="row mt-5">
+        <ShowTask setShowMessage={setShowMessage}/>
+      </div>
     </div>
   );
 };
