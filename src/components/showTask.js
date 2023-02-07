f51918e186e03b22077209365698b4f9fbe87ef9
GayanMalinda99 <gayan1999malinda@gmail.com>
2023-02-07 13:34:49 +0530

    Add styles and fixed showTask state problem

--- src/components/showTask.js
+++ src/components/showTask.js
@@ -1,15 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "../asserts/styles/showTask.css";
 import { useSelector, useDispatch } from "react-redux";
-import { 
-  deleteTask, 
-  getAllTasks, 
-  setEdit 
-} from "../actions/taskAction";
+import { deleteTask, getAllTasks, setEdit } from "../actions/taskAction";
 import { Link } from "react-router-dom";
 
 const ShowTask = ({ setShowMessage }) => {
-  const { tasks, edit } = useSelector((state) => state);
+  const { tasks, edit } = useSelector((state) => state.tasksList);
   const [clicked, setClicked] = useState(false);
   const dispatch = useDispatch();
 
@@ -34,44 +30,48 @@ const ShowTask = ({ setShowMessage }) => {
   useEffect(() => {
     dispatch(getAllTasks());
   }, []);
-
+  console.log(tasks);
   return (
     <>
       {tasks.length !== 0 && (
         <div className="showtask">
           {tasks.map((task, index) => {
             <div className="task m-3" onClick={handleClick}>
-            <div className="d-flex " style={{ alignItems: "center" }} key={index}>
-              <span className="ml-1" style={{ alignItems: "center" }}>
-                {task.title}
-              </span>
-              <div className="buttondiv">
-                <Link to="/tasks/add">
+              <div
+                className="d-flex "
+                style={{ alignItems: "center" }}
+                key={index}
+              >
+                <span className="ml-1" style={{ alignItems: "center" }}>
+                  {task.title}
+                </span>
+                <div className="button-div">
+                  <Link to="/tasks/add">
+                    <button
+                      type="button"
+                      className="btn btn-success btn-sm m-2 px-3"
+                      id={task.id}
+                      onClick={handleEdit}
+                    >
+                      Edit
+                    </button>
+                  </Link>
                   <button
                     type="button"
-                    className="btn btn-success btn-sm m-2 px-3"
+                    className="btn btn-danger btn-sm m-2"
                     id={task.id}
-                    onClick={handleEdit}
+                    onClick={handleDelete}
                   >
-                    Edit
+                    Delete
                   </button>
-                </Link>
-                <button
-                  type="button"
-                  className="btn btn-danger btn-sm m-2"
-                  id={task.id}
-                  onClick={handleDelete}
-                >
-                  delete
-                </button>
+                </div>
               </div>
-            </div>
-            {clicked && (
-              <div className="ml-1" style={{ margineLeft: "5%" }}>
-                {task.description}
-              </div>
-            )}
-          </div>
+              {clicked && (
+                <div className="ml-1" style={{ margineLeft: "5%" }}>
+                  {task.description}
+                </div>
+              )}
+            </div>;
           })}
         </div>
       )}
@@ -79,4 +79,4 @@ const ShowTask = ({ setShowMessage }) => {
   );
 };
 
-export default ShowTask
+export default ShowTask;
